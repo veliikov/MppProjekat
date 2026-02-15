395e8a87e50db18d08e8dab3be986eff220dae37
veliikov <nvelikov9@gmail.com>
2026-02-15 20:13:23 +0100

    Player komponenta i promena stilizacije

--- OneDrive/Радна површина/MppProjekat/src/App.jsx
+++ OneDrive/Радна површина/MppProjekat/src/App.jsx
@@ -1,3 +1,4 @@
+import Player from "./components/Player"
 
 function App() {
   
@@ -6,14 +7,9 @@ function App() {
     <main>
       <div id="game-container">
         <ol id="players">
-          <li>
-            <span className="player-name">Player 1</span>
-            <span className="player-symbol">X</span>
-          </li>
-          <li>
-             <span className="player-name">Player 2</span>
-            <span className="player-symbol">O</span>
-          </li>
+         <Player playerName="Player 1" symbol="X"/>
+          
+          <Player playerName="Player 2" symbol="O"/>
         </ol>
       </div>
     </main>
---
+++ OneDrive/Радна површина/MppProjekat/src/components/Player.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react'
+
+const Player = ({playerName,symbol}) => {
+
+    const [isEditing,setIsEditing] = useState(false);
+    const [name,setName] = useState(playerName);
+
+    function handleEditing() {
+        setIsEditing(prev =>!prev);   
+    }
+
+    function handleChangeName(event) {
+        setName(event.target.value);
+    }
+
+    let nameComponent =  <span className="player-name">{name}</span>;
+    if (isEditing) {
+        nameComponent=<input type="text" required value={name} onChange={handleChangeName} />
+    }
+
+  return (
+    <li>
+            <span className="player">
+           {nameComponent}
+            <span className="player-symbol">{symbol}</span>
+            </span>
+            <button onClick={handleEditing}>{isEditing ? 'Save' : 'Edit'}</button>
+          </li>
+  )
+}
+
+export default Player
