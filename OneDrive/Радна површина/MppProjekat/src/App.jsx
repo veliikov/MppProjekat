d01d90b000292e03b8d2441b8d77e6668bb2a52b
lukav02 <lukav0311@gmail.com>
2026-02-16 11:44:03 +0100

    Kombinacije dodate i pobednik logika

--- OneDrive/Радна површина/MppProjekat/src/App.jsx
+++ OneDrive/Радна површина/MppProjekat/src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import GameBoard from "./components/GameBoard";
 import Player from "./components/Player";
 import Log from "./components/Log";
+import { WINNING_COMBINATIONS } from "./winning-combination";
 
 function deriveActivePlayer(gameTurns) {
   let currentPlayer = "X";
@@ -11,9 +12,39 @@ function deriveActivePlayer(gameTurns) {
   return currentPlayer;
 }
 
+const initialGB = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+];
+
 function App() {
   const [gameTurns, setGameTurns] = useState([]);
   const activePlayer = deriveActivePlayer(gameTurns);
+
+  let gameBoard = initialGB;
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn;
+    const { row, col } = square;
+    gameBoard[row][col] = player;
+  }
+
+  let winner = undefined;
+
+  for (const comb of WINNING_COMBINATIONS) {
+    const firstSquareSym = gameBoard[comb[0].row][comb[0].column];
+    const secondSquareSym = gameBoard[comb[1].row][comb[1].column];
+    const thirdSquareSym = gameBoard[comb[2].row][comb[2].column];
+
+    if (
+      firstSquareSym &&
+      firstSquareSym === secondSquareSym &&
+      firstSquareSym === thirdSquareSym
+    ) {
+      winner = firstSquareSym;
+    }
+  }
 
   function handleSelectSquare(row, col) {
     setGameTurns((prev) => {
@@ -41,9 +72,10 @@ function App() {
             isActive={activePlayer === "O"}
           />
         </ol>
-        <GameBoard turns={gameTurns} onSelectSquare={handleSelectSquare} />
+        {winner && <p>You Won, {winner}!</p>}
+        <GameBoard board={gameBoard} onSelectSquare={handleSelectSquare} />
       </div>
-     {gameTurns.length>0 && <Log turns={gameTurns} />}
+      {gameTurns.length > 0 && <Log turns={gameTurns} />}
     </main>
   );
 }
--- OneDrive/Радна површина/MppProjekat/src/components/GameBoard.jsx
+++ OneDrive/Радна површина/MppProjekat/src/components/GameBoard.jsx
@@ -1,23 +1,9 @@
 import React from "react";
 
-const initialGB = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-const GameBoard = ({ onSelectSquare, turns }) => {
-  let gameBoard = initialGB;
-
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-    gameBoard[row][col] = player;
-  }
-
+const GameBoard = ({ onSelectSquare, board }) => {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((playerSymbol, colIndex) => (
